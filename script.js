cd900f3502599e09e2d77fb20c5659afd8b1f579
krisztapetras <92876874+krisztapetras@users.noreply.github.com>
2021-11-17 16:20:01 +0000

    Add files for lab 8

---
+++ script.js
@@ -0,0 +1,55 @@
+var computerChoiceDisplay=document.getElementById('computer-choice')
+var userChoiceDisplay=document.getElementById('user-choice')
+var resultDisplay=document.getElementById('result')
+var possibleChoices=document.querySelectorAll('button')
+var userChoice
+var computerChoice
+var result
+
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click',(e)=>{
+	userChoice = e.target.id
+	userChoiceDisplay.innerHTML=userChoice
+	generateComputerChoice()
+	getResult()
+}))
+
+function generateComputerChoice(){
+	var randomNumber = Math.floor(Math.random()*3)+1
+	
+	if (randomNumber===1){
+		computerChoice='rock'
+	}
+	if (randomNumber===2){
+		computerChoice='scissors'
+	}
+	if(randomNumber===3){
+		computerChoice='paper'
+	}
+	computerChoiceDisplay.innerHTML= computerChoice
+	
+}
+
+function getResult(){
+	if(computerChoice===userChoice){
+		result='its a draw'
+	}
+	if(computerChoice==='rock' && userChoice==="paper"){
+		result='you win!'
+	}
+	if (computerChoice==='rock' && userChoice==="scissors"){
+		result='you lost!'
+	}
+	if (computerChoice==='paper' && userChoice==="scissors"){
+		result='you win!'
+	}
+	if (computerChoice==='paper' && userChoice==="rock"){
+		result='you lose!'
+	}
+	if (computerChoice==='scissors'&& userChoice==="paper"){
+		result='you lost!'
+	}
+	if (computerChoice==='scissors' && userChoice==="rock"){
+		result='you win!'
+	}
+	resultDisplay.innerHTML= result
+}
